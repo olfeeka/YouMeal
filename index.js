ab40f69142c03fc09ec5564620daa5ac1d4f56f9
olfeeka <olfert90@gmail.com>
2023-08-16 12:59:45 +0400

    доделки по корзине

--- index.js
+++ index.js
@@ -139,8 +139,110 @@ displayProducts(1);
 
 
 
-// отображение popup продукта при нажатии на картинку
+/* "КОРЗИНА" start */ // Екатерина
+let cartItems = []; // массив для хранения товаров в корзине
+//const cartContainer = document.querySelector(".main__basket");
+const cartList = document.querySelector(".cart-list");
+const cartTotal = document.querySelector(".cart-total");
+const cartButton = document.querySelector(".main__basket-btn");
 
+
+// функция отрисовки карточки в корзине
+function displayCart() {
+  cartList.innerHTML = "";
+
+  cartItems.forEach((product) => {
+    const quantity = product.quantity;
+
+    cartList.innerHTML += `
+    <div class="cart-list__item">
+    <img class="cart-list__img" src="${product.image_url}" alt="${product.name}"/>
+    <div class="cart-list__info"
+    <p class="cart-list__name">${product.name}</p>
+    <p class="cart-list__weight">${product.energy}</p>
+    <p class="cart-list__price">${product.price}</p>
+    </div>
+    <div class="cart-list__counter">
+    <button class="cart-list__btn-minus" onclick="decreaseQuantity(${product.id})">-</button>
+    <span class="cart-list__quantity">${quantity}</span>
+    <button class="cart-list__btn-plus" onclick="increaseQuantity(${product.id})">+</button>
+    </div>
+    </div>`;
+  });
+
+  if (cartItems.length > 0) {
+    const totalPrice = cartItems.reduce((total, product) => total + (parseFloat(product.price) * product.quantity), 0);
+
+    cartTotal.innerHTML = `
+    <p>Итого</p>
+    <p>${totalPrice}₽</p>`;
+
+    cartButton.style.display = "block";
+  } else {
+    cartTotal.innerHTML = "";
+    cartButton.style.display = "none";
+  }
+  
+  updateCartQuantity();
+}
+
+
+// функция для добавления товара в корзину
+function addToCart(productId) {
+  const product = filteredProducts.find((product) => product.id === productId); // находим товар с помощью id
+
+  const existingProduct = cartItems.find((product) => product.id === productId); // проверяем, есть ли такой товар уже в корзине
+  if (existingProduct) {
+    existingProduct.quantity += 1; // если товар уже есть - увеличиваем его количество на 1
+  } else {
+    cartItems.push({ ...product, quantity: 1 }); // если товара нет - добавляем его со значением количества 1
+  }
+
+  // отрисовываем корзину
+  displayCart();
+}
+
+
+// функция для увеличения количества товара в корзине
+function increaseQuantity(productId) {
+  const product = cartItems.find((product) => product.id === productId); // находим товар в корзине
+
+  product.quantity += 1; // увеличиваем количество товара на 1
+
+  displayCart(); // обновляем отображение корзины
+}
+
+
+// функция для уменьшения количества товара в корзине
+function decreaseQuantity(productId) {
+  const product = cartItems.find((product) => product.id === productId); // находим товар в корзине
+
+  if (product.quantity === 1) {
+    cartItems = cartItems.filter((item) => item.id !== productId); // проверяем, если кол-во равно 1, то удаляем его из корзины
+  } else {
+    product.quantity -= 1; // иначе уменьшаем количество товара на 1
+  }
+
+  displayCart(); // обновляем отображение корзины
+}
+
+
+// функция для обновления количества товаров в корзине
+function updateCartQuantity() {
+  const basketQuantity = document.querySelector(".main__basket-quantity");
+  const totalQuantity = cartItems.reduce(
+    (total, product) => total + product.quantity,
+    0
+  );
+  basketQuantity.textContent = totalQuantity;
+}
+
+/* "КОРЗИНА" end */
+
+
+
+/* ПОПАП КАРТОЧКИ с подробной инфой start */ // Марина
+// отображение popup продукта при нажатии на картинку
 const productCard_conteiner  = document.querySelector(".product-card_conteiner");
 
 async function displayProductsCard(id) {
@@ -224,100 +326,4 @@ minus.addEventListener("click", ()=>{
         number.innerHTML=a;}
         )
 
-
-
-/* "КОРЗИНА" start */
-let cartItems = []; // массив для хранения добавленных товаров
-const cartList = document.querySelector(".cart-list");
-const cartTotal = document.querySelector(".cart-total");
-
-
-// функция отрисовки карточки в корзине
-function displayCart() {
-  cartList.innerHTML = "";
-
-  cartItems.forEach((product) => {
-    const quantity = product.quantity;
-
-    cartList.innerHTML += `
-    <div class="cart-list__item">
-    <img class="cart-list__img" src="${product.image_url}" alt="${product.name}"/>
-    <div class="cart-list__info"
-    <p class="cart-list__name">${product.name}</p>
-    <p class="cart-list__weight">${product.energy}</p>
-    <p class="cart-list__price">${product.price}</p>
-    </div>
-    <div class="cart-list__counter">
-    <button class="cart-list__btn-minus" onclick="decreaseQuantity(${product.id})">-</button>
-    <span class="cart-list__quantity">${quantity}</span>
-    <button class="cart-list__btn-plus" onclick="increaseQuantity(${product.id})">+</button>
-    </div>
-    </div>`;
-  });
-
-  if (cartItems.length > 0) {
-    const totalPrice = cartItems.reduce((total, product) => total + (parseFloat(product.price) * product.quantity), 0);
-
-    cartTotal.innerHTML = `
-    <p>Итого</p>
-    <p>${totalPrice}₽</p>
-    <button class="cart-total__btn" onclick="checkout()">Оформить заказ</button>`;
-  } else {
-    cartTotal.innerHTML = "";
-  }
-  
-  updateCartQuantity();
-}
-
-
-// функция для добавления товара в корзину
-function addToCart(productId) {
-  const product = filteredProducts.find((product) => product.id === productId); // находим товар с помощью id
-
-  const existingProduct = cartItems.find((product) => product.id === productId); // проверяем, есть ли такой товар уже в корзине
-  if (existingProduct) {
-    existingProduct.quantity += 1; // если товар уже есть - увеличиваем его количество на 1
-  } else {
-    cartItems.push({ ...product, quantity: 1 }); // если товара нет - добавляем его со значением количества 1
-  }
-
-  // отрисовываем корзину
-  displayCart();
-}
-
-
-// функция для увеличения количества товара в корзине
-function increaseQuantity(productId) {
-  const product = cartItems.find((product) => product.id === productId); // находим товар в корзине
-
-  product.quantity += 1; // увеличиваем количество товара на 1
-
-  displayCart(); // обновляем отображение корзины
-}
-
-
-// функция для уменьшения количества товара в корзине
-function decreaseQuantity(productId) {
-  const product = cartItems.find((product) => product.id === productId); // находим товар в корзине
-
-  if (product.quantity === 1) {
-    cartItems = cartItems.filter((item) => item.id !== productId); // проверяем, если кол-во равно 1, то удаляем его из корзины
-  } else {
-    product.quantity -= 1; // иначе уменьшаем количество товара на 1
-  }
-
-  displayCart(); // обновляем отображение корзины
-}
-
-
-// функция для обновления количества товаров в корзине
-function updateCartQuantity() {
-  const basketQuantity = document.querySelector(".main__basket-quantity");
-  const totalQuantity = cartItems.reduce(
-    (total, product) => total + product.quantity,
-    0
-  );
-  basketQuantity.textContent = totalQuantity;
-}
-
-/* "КОРЗИНА" end */
+/* ПОПАП КАРТОЧКИ с подробной инфой end */
